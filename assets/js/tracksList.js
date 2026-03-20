6f59a34f035c10ada9a5b1d7a21a74768a99a759
Michel Armani <79829928+MichelArmani@users.noreply.github.com>
2026-03-20 01:58:28 -0400

    Actualización de tracks vía panel

--- assets/js/tracksList.js
+++ assets/js/tracksList.js
@@ -1,5 +1,5 @@
 // Archivo generado automáticamente por el Panel de Administración
-// Última actualización: 2026-03-20 01:58:07
+// Última actualización: 2026-03-20 01:58:26
 
 function getTracks() {
     return [
