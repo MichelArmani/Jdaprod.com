0fbcce364088e91a1072cf0767b4fac8690c4b80
Michel Armani <79829928+MichelArmani@users.noreply.github.com>
2026-03-20 02:15:01 -0400

    Actualización de tracks vía panel

--- assets/js/tracksList.js
+++ assets/js/tracksList.js
@@ -1,11 +1,11 @@
 // Archivo generado automáticamente por el Panel de Administración
-// Última actualización: 2026-03-20 02:06:08
+// Última actualización: 2026-03-20 02:14:58
 
 function getTracks() {
     return [
         {
             id: 1,
-            title: "I Remember Everythinggg",
+            title: "I Remember Everything",
             artist: "J DaProd",
             duration: "",
             preview: "../assets/songs/remember.mp3",
