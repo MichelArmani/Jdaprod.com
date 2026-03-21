829db09f599fd0247acd59063fa8abe74b5b48af
Michel Armani <79829928+MichelArmani@users.noreply.github.com>
2026-03-21 01:40:14 -0400

    Update tracksList.js

--- assets/js/tracksList.js
+++ assets/js/tracksList.js
@@ -88,14 +88,6 @@ function getTracks() {
             basicPaypalButton: "XMMGD24J4K9CA",
             premiumFileForDownload: "assets/downloads/premium/dnlkqi/PREMIUM - Kehlani.zip",
             basicFileForDownload: "assets/downloads/basic/lqkwei/BASIC - Kehlani.zip",
-        },
-        {
-            id: 9,
-            title: "Cancion de Prueba",
-            artist: "J DaProd",
-            duration: "3:15",
-            preview: "https://file-examples.com/wp-content/storage/2017/11/file_example_MP3_700KB.mp3",
-            type: "production",
         }
     ];
 }
